ca9c1bc5995bd8b2d191d5d6770ae800c6d20a15
Manish Bhagat <126966039+MANISH230620@users.noreply.github.com>
2024-12-12 19:02:52 +0530

    Update Home.jsx
    updated acc to me

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
                 <div className='mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl  lg:text-6xl dark:text-white '>Location based chatbot based on your interest</div>
             </div>
             <div className='flex gap-5 m-10 flex-wrap justify-center'>
-                <Card name="chatbot for Schemes" desc="Tells you about schemes available all  over india" image="https://i.ytimg.com/vi/UHCxrI7UExU/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB4QnoPVakrf3V-Wsc1kWuYLVosWA" tag1="India" tag2="Farmers scheme" btnLink="/chat/india"></Card>
-                <Card name="chatbot for General Query" desc="Tells you about schemes " image="https://bep-wordpress.byjusexamprep.com/wp-content/uploads/2023/08/28170756/external-image-7217.jpg" tag1="General Query" tag2="Farmers scheme" btnLink="/chat/india"></Card>
+                <Card name="chatbot for farmers" desc="Tells you about schemes available all  over india" image="https://imgs.search.brave.com/oByJMBbnFyHe66Qe9A7Txi68QBv_M2PN2j0KMYvH0ok/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMub2Rpc2hhdHYu/aW4vdXBsb2FkaW1h/Z2UvbGlicmFyeS8x/Nl85LzE2XzlfMC9y/ZWNlbnRfcGhvdG9f/MTY4MTQ1NzY3NS53/ZWJw" tag1="India" tag2="Farmers scheme" btnLink="/chat/india"></Card>
+                <Card name="chatbot for medical query" desc="Resolve your query for medical schemes " image="https://imgs.search.brave.com/tN_Ie_lJy7kPvU-H3WEczt5mrsBNCxwgL2OlfHaZ1Jg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kMmp4/MnJlcnJnNnNoMy5j/bG91ZGZyb250Lm5l/dC9pbWFnZXMvQXJ0/aWNsZV9JbWFnZXMv/SW1hZ2VGb3JBcnRp/Y2xlXzIyNDU3XzE2/NTE2Nzg4NzMwNDcz/MjQ5LmpwZw" tag1="Medical" tag2="schemes" btnLink="/chat/india"></Card>
 
             </div>
 
